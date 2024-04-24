467c755f17ae6d8b39748e868a06317e6fdbfa0d
Oceane BOUNAN <bounan.oceane@orange.fr>
2024-04-24 18:22:48 +0400

    fixed refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :restaurants, only: %i[index show new create] do
-    resources :reviews, only: %i[new create]
+    resources :reviews, only: %i[create]
   end
 end
